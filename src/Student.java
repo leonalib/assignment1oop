b0584ac621fbf26d345797fb1c5a6c5b5c6a580d
leonalib <ayrkinbek@gmail.com>
2025-12-28 21:25:49 +0500

    first assignment done

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+class Student extends LibraryUser {
+
+    public Student(int UserId, String Name) {
+        super(UserId, Name);
+    }
+
+    @Override
+    public String getRole() {
+        return "Student";
+    }
+}
